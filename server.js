c70c2ab3140018a4e9b6b4ce7051c77bbf1c3bdc
Anas Qazi <ianasqazi@gmail.com>
2020-02-13 17:38:06 -0500

    added favicon

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+
 
 const app = express();
 
