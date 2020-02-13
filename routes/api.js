e63e2aa46b4dcdebdeac849328f1929b48960248
Anas Qazi <ianasqazi@gmail.com>
2020-02-13 17:42:43 -0500

    added slash

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
+router.get("/", (req, res) => {
+  Transaction.find({}).sort({date: -1})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
